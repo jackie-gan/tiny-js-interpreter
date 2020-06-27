2f2aea14fb96bf3fdb833d93def0fc03fef0cc06
jackie-gan <jackiegan405679096@gmail.com>
2020-06-27 23:46:53 +0800

    fix: update types

--- src/scope.ts
+++ src/scope.ts
@@ -1,8 +1,26 @@
+import { ScopeType, VariableObject } from '../types/type';
+
 /**
  * 形成执行上下文的三种情况：全局作用域、函数作用域、eval
  * 
  * 执行上下文中，会产生作用域Scope
  */
 export class Scope {
+  private parent: Scope | null;
+  private content: { [key: string]: VariableObject };
+  public invasive: boolean;
 
+  constructor(public readonly type: ScopeType, parent?: Scope) {
+    this.parent = parent || null;
+    this.content = {};
+    this.invasive = false;
+  }
+
+  public var(name: string, value: any) {
+    this.content[name] = value;
+
+    if () {
+
+    }
+  }
 }
---
+++ types/type.ts
@@ -0,0 +1,5 @@
+export type ScopeType = 'function' | 'block' | 'loop' | 'switch';
+
+export interface VariableObject {
+
+}
