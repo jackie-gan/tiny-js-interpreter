2b58ef972a598f6ec7865934ec428de43fbaa77a
jackie-gan <jackiegan405679096@gmail.com>
2020-06-29 12:40:57 +0800

    feat: add externalApi

--- src/execute.ts
+++ src/execute.ts
@@ -3,12 +3,16 @@ import { evaluate } from './evaluate';
 import { Scope } from './scope'; 
 import { defaultApis } from './utils';
 
-export function execute(code: string) {
+export function execute(code: string, externalApis: any = {}) {
   const scope = new Scope('root');
   scope.const('this', null);
 
   for (const name of Object.getOwnPropertyNames(defaultApis)) {
     scope.const(name, defaultApis[name]);
+  }
+
+  for (const name of Object.getOwnPropertyNames(externalApis)) {
+    scope.const(name, externalApis[name]);
   }
 
   return evaluate(acorn.parse(code, {
